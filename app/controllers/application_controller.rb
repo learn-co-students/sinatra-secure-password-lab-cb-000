d6f69e680fc93053eb28e92e0bba049392f97bcc
adam n <adam.nawroc@gmail.com>
2018-03-27 14:01:40 +0000

    progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 require "./config/environment"
 require "./app/models/user"
+
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -19,6 +21,13 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
 
+    user = User.new(:username => params[:username], :password => params[:password])
+    # binding.pry
+    if user.save
+        redirect "/login"
+    else
+        redirect "/failure"
+    end
   end
 
   get '/account' do
@@ -33,6 +42,15 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.find_by(:username => params[:username])
+    # if user && user.authenticate(params[:password])
+    if user
+        # session[:user_id] = user.id
+        # maybe not working w/o the sesh
+        redirect "/account"
+    else
+        redirect "/failure"
+    end
   end
 
   get "/failure" do
