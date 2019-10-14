011701c23759b796f57948a67c28787ef2362702
Will Hodgson <will.hodgson@current-rms.com>
2019-10-14 15:00:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,13 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-
+    if params[:username] != "" && params[:password] != ""
+      user = User.new(:username => params[:username], :password => params[:password])
+      user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
   end
 
   get '/account' do
@@ -32,7 +38,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    if params[:username] != "" && params[:password] != ""
+      user = User.find_by(:username => params[:username])
+		  if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+			  redirect "/account"
+      end
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
